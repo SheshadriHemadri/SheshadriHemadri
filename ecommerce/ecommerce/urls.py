ea7ab1d7aba3083a78f35ec946c5939e79bd94d2
SheshadriHemadri <thulasiramsheshadri@gmail.com>
2025-07-12 15:37:18 +0530

    Add files via upload

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for mothertheresa project.
+URL configuration for ecommerce project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -21,9 +21,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('canteen.urls')),
+    path('', include('catalog.urls')),
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
